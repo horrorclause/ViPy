8210b9c05a7cec91ecfa9041ddc951b8f3dba20a
Mario <mjere003@gmail.com>
2024-12-29 16:14:34 -0500

    Create TCP and UDP Clients

---
+++ simpleTCPClient.py
@@ -0,0 +1,19 @@
+import socket
+
+target_host = "www.example.com"
+target_port = 80
+
+#Create a socket object
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+#Connect to the client
+client.connect((target_host, target_port))
+
+#Send some data
+client.send(b"GET / HTTP/1.1\r\nHost: example.com\r\n\r\n")
+
+#Receive some data
+response=client.recv(4096)
+
+print(response.decode())
+client.close()
---
+++ simpleUDPClient.py
@@ -0,0 +1,16 @@
+import socket
+
+targetHost = "127.0.0.1"
+targetPort = 50001
+
+# Create a socket object, use DGRAM for datagram (UDP)
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# Send some data
+client.sendto(b'AAAAAABBBBBCCCCCCC', (targetHost, targetPort))
+
+# Receive some data
+data, addr = client.recvfrom(4096)
+
+print(data.decode())
+client.close()
