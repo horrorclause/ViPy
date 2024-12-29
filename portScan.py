4476d463c83443d4a35a2f80a937cfd61d2c75a4
Mario <mjere003@gmail.com>
2024-12-29 14:57:34 -0500

    creation of optParse

--- portScan.py
+++ portScan.py
@@ -1,7 +1,16 @@
 import optparse
 
 
+# Uses optParse to instruct on script usage
 parser = optparse.OptionParser('usage %prog -H ' + '<target host> -p <target port>')
 parser.add_option('-H', dest='tgtHost', type='string', help='specify target host')
+parser.add_option('-p', dest='tgtPort', type='int', help='specify target port')
+(options, args) = parser.parse_args()
+tgtHost=options.tgtHost
+tgtPort=options.tgtPort
+
+if (tgtHost == None) | (tgtPort == None):
+    print(parser.usage)
+    exit(0)
 
 print(parser.usage)
